42705d8abcb8432839196ceec00079c9a743c685
Anil Dahiya <adahiya@ford.com>
2015-11-21 18:04:44 +0530

    Changes for better handling App Resumption scenario.

--- sdl_android_lib/src/com/smartdevicelink/proxy/SdlProxyBase.java
+++ sdl_android_lib/src/com/smartdevicelink/proxy/SdlProxyBase.java
@@ -1710,13 +1710,13 @@ public abstract class SdlProxyBase<proxyListenerType extends IProxyListenerBase>
 
 					if (_bAppResumeEnabled)
 					{
-						if ( (msg.getResultCode() == Result.RESUME_FAILED) || (msg.getResultCode() != Result.SUCCESS) )
+						if ( (_sdlMsgVersion.getMajorVersion() > 2) && (_lastHashID != null) && (msg.getSuccess()) && (msg.getResultCode() != Result.RESUME_FAILED) )
+							_bResumeSuccess = true;
+						else
 						{
 							_bResumeSuccess = false;
 							_lastHashID = null;
 						}
-						else if ( (_sdlMsgVersion.getMajorVersion() > 2) && (_lastHashID != null) && (msg.getResultCode() == Result.SUCCESS) )
-							_bResumeSuccess = true;				
 					}
 					_diagModes = msg.getSupportedDiagModes();
 					
@@ -1858,18 +1858,18 @@ public abstract class SdlProxyBase<proxyListenerType extends IProxyListenerBase>
 				_vehicleType = msg.getVehicleType();
 				_audioPassThruCapabilities = msg.getAudioPassThruCapabilities();
 				_proxyVersionInfo = msg.getProxyVersionInfo();
-				
+
 				if (_bAppResumeEnabled)
 				{
-					if ( (msg.getResultCode() == Result.RESUME_FAILED) || (msg.getResultCode() != Result.SUCCESS) )
+					if ( (_sdlMsgVersion.getMajorVersion() > 2) && (_lastHashID != null) && (msg.getSuccess()) && (msg.getResultCode() != Result.RESUME_FAILED) )
+						_bResumeSuccess = true;
+					else
 					{
 						_bResumeSuccess = false;
 						_lastHashID = null;
 					}
-					else if ( (_sdlMsgVersion.getMajorVersion() > 2) && (_lastHashID != null) && (msg.getResultCode() == Result.SUCCESS) )
-						_bResumeSuccess = true;				
-				}						
-				
+				}
+
 				_diagModes = msg.getSupportedDiagModes();				
 				
 				if (!isDebugEnabled()) 
