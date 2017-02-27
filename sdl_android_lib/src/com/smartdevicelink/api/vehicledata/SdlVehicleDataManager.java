b2a87bc9c080baeafd83b15aef238d33f74f3382
Mark Schwerzler <mschwerz@MGC000001540>
2017-02-27 13:34:46 -0500

    Vehicle data subscription rpcs will be sent once they are requested and once the permission becomes available

--- sdl_android_lib/src/com/smartdevicelink/api/vehicledata/SdlVehicleDataManager.java
+++ sdl_android_lib/src/com/smartdevicelink/api/vehicledata/SdlVehicleDataManager.java
@@ -1,15 +1,13 @@
 package com.smartdevicelink.api.vehicledata;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.smartdevicelink.api.interfaces.SdlContext;
 import com.smartdevicelink.proxy.RPCMessage;
-import com.smartdevicelink.proxy.rpc.GetVehicleData;
 import com.smartdevicelink.proxy.rpc.GetVehicleDataResponse;
 import com.smartdevicelink.proxy.rpc.OnVehicleData;
-import com.smartdevicelink.proxy.rpc.SubscribeVehicleData;
-import com.smartdevicelink.proxy.rpc.UnsubscribeVehicleData;
 
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -19,12 +17,12 @@ public class SdlVehicleDataManager {
     private static final String TAG = SdlVehicleDataManager.class.getSimpleName();
 
     private HashMap<SdlDataEnums, Object> mCurrentData = new HashMap<>();
-    private final SdlContext mContext;
     private HashMap<SdlDataEnums,CopyOnWriteArrayList<SdlVehicleDataListener>> mVehDataListenerMap = new HashMap<>();
     private final Object DATA_LOCK = new Object();
+    private final SdlVehicleDataPermissions mPermissions;
 
     public SdlVehicleDataManager(SdlContext context){
-        mContext = context;
+        mPermissions = new SdlVehicleDataPermissions( context, context.getSdlPermissionManager());
     }
 
     public @Nullable Object getVehicleData(SdlDataEnums dataEnum){
@@ -33,57 +31,38 @@ public class SdlVehicleDataManager {
         }
     }
 
-    public boolean subscribeVehicleData(final SdlDataEnums dataEnum, SdlVehicleDataListener listener){
+    public void subscribeVehicleData(@NonNull final SdlDataEnums dataEnum, @NonNull SdlVehicleDataListener listener){
         CopyOnWriteArrayList<SdlVehicleDataListener> dataListeners = mVehDataListenerMap.get(dataEnum);
         if(dataEnum == SdlDataEnums.VIN){
-            Log.w(TAG, "VIN is not supported for subscription");
-            return false;
+            throw new RuntimeException("VIN is not supported for Vehicle data subscription");
         }
-        if(dataListeners==null){
+        if(dataListeners == null){
             dataListeners = new CopyOnWriteArrayList<>();
             mVehDataListenerMap.put(dataEnum,dataListeners);
         }
-        if(dataListeners.isEmpty() &&
-                mContext.getSdlPermissionManager().isPermissionAvailable(dataEnum.getSubVehicleDataPermission())){
-            SubscribeVehicleData request = new SubscribeVehicleData();
-            request.setParameters(dataEnum.getKeyName(),true);
-            mContext.sendRpc(request);
-            dataListeners.add(listener);
-            return true;
-        }else if(!dataListeners.isEmpty()){
-            dataListeners.add(listener);
-            return true;
-        } else{
-            return false;
+        if(dataListeners.isEmpty()){
+            mPermissions.subscribeVehicleData(dataEnum);
         }
+        dataListeners.add(listener);
     }
 
-    public boolean unsubscribeVehicleData(SdlDataEnums dataEnum, SdlVehicleDataListener listener){
+    public void unsubscribeVehicleData(@NonNull SdlDataEnums dataEnum, @NonNull SdlVehicleDataListener listener){
         CopyOnWriteArrayList<SdlVehicleDataListener> dataListeners = mVehDataListenerMap.get(dataEnum);
+        if(dataEnum == SdlDataEnums.VIN){
+            throw new RuntimeException("VIN is not supported for Vehicle data unsubscription");
+        }
         if(dataListeners==null || dataListeners.isEmpty()){
             Log.w(TAG,"Unable to unsubscribe because there were no listeners subscribed to the data.");
-            return false;
+            return;
         }
         boolean listenerRemoved = dataListeners.remove(listener);
-        if(dataListeners.isEmpty() &&
-                mContext.getSdlPermissionManager().isPermissionAvailable(dataEnum.getUnsubVehicleDataPermission())){
-                UnsubscribeVehicleData request = new UnsubscribeVehicleData();
-                request.setParameters(dataEnum.getKeyName(),true);
-                mContext.sendRpc(request);
-
+        if(dataListeners.isEmpty() && listenerRemoved){
+            mPermissions.unsubscribeVehicleData(dataEnum);
         }
-        return listenerRemoved;
     }
 
-    public boolean pullVehicleData(SdlDataEnums dataEnum){
-        if(mContext.getSdlPermissionManager().isPermissionAvailable(dataEnum.getGetVehicleDataPermission())){
-            GetVehicleData request = new GetVehicleData();
-            request.setParameters(dataEnum.getKeyName(),true);
-            mContext.sendRpc(request);
-            return true;
-        } else {
-            return false;
-        }
+    public boolean pullVehicleData(@NonNull SdlDataEnums dataEnum){
+        return mPermissions.sendVehiclePull(dataEnum);
     }
 
     public void OnGetVehicleData(GetVehicleDataResponse data){
@@ -99,8 +78,6 @@ public class SdlVehicleDataManager {
             }
         }
     }
-
-
 
     private EnumSet<SdlDataEnums> updateData(RPCMessage data){
         synchronized (DATA_LOCK){
